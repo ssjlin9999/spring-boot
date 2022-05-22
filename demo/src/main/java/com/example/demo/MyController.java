7fdacb723810a46810720f33092395c7a4def80a
Sunny Lin <sunnylin9999@gmail.com>
2022-05-22 22:39:38 +0800

    Add DI sample when having two Bean
    DI = Dependency Injection
    annotation: @Autowired  + @Qualifier("canonPrinter")

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
     @Autowired
+    @Qualifier("canonPrinter")
     private Printer printer;
 
     @RequestMapping("/test")
