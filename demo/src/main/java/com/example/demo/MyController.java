64018e073cbb40abb57ddfcd2ebb82854a6dd5f5
Sunny Lin <sunnylin9999@gmail.com>
2022-05-22 22:15:22 +0800

    Add IoC sample
    IoC = Inversion of Control
    annotation: @Component, @Autowired

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -1,14 +1,18 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
+    @Autowired
+    private Printer printer;
+
     @RequestMapping("/test")
     public String test() {
-        System.out.println("Hi");
+        printer.print("Hi printer");
         return "Hello World";
     }
 }
