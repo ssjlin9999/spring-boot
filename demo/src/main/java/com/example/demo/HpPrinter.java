a73bf9d0749383f25f65595a8d95d34ab1cf8128
Sunny Lin <sunnylin9999@gmail.com>
2022-06-12 23:01:19 +0800

    Add AOP pointcut method: solution 1
    annotation: @Aspect + @Component + @Before + @After
    
    Add dependency to pom.xml: spring-boot-starter-aop

--- demo/src/main/java/com/example/demo/HpPrinter.java
+++ demo/src/main/java/com/example/demo/HpPrinter.java
@@ -1,21 +1,12 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HpPrinter implements Printer {
 
-    @Value("${printer.name}")
-    private String name;
-
-    @Value("${printer.count:20}")
-    private int count;
-
     @Override
     public void print(String message) {
-        count--;
-        System.out.println(name + ": " + message);
-        System.out.println("Remaining usage: " + count);
+        System.out.println("HP Printer: " + message);
     }
 }
