48b3cb3592c2d718067a66597f5b81a87ec951b3
Sunny Lin <sunnylin9999@gmail.com>
2022-06-12 23:01:44 +0800

    Add AOP pointcut method: solution 2
    annotation: @Aspect + @Component + @Around
    
    Add dependency to pom.xml: spring-boot-starter-aop

--- demo/src/main/java/com/example/demo/MyAspect.java
+++ demo/src/main/java/com/example/demo/MyAspect.java
@@ -1,21 +1,22 @@
 package com.example.demo;
 
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyAspect {
 
-    @Before("execution(* com.example.demo.HpPrinter.*(..))")
-    public void before() {
+    @Around("execution(* com.example.demo.HpPrinter.*(..))")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
         System.out.println("I'm before");
-    }
 
-    @After("execution(* com.example.demo.HpPrinter.*(..))")
-    public void after() {
+        // Execute pointcut's method
+        Object obj = pjp.proceed();
+
         System.out.println("I'm after");
+        return obj;
     }
 }
