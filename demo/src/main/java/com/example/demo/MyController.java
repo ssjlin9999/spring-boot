ef0370c6fc2ac83e11afb6672ff3520fa9082980
Sunny Lin <sunnylin9999@gmail.com>
2022-06-19 23:43:28 +0800

    Add Store and Student object for API return
    Using Json format

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -3,17 +3,26 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/detail")
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class MyController {
 
     @RequestMapping("/product")
-    public String product() {
-        return "1.Apple 2.Orange";
+    public Store product() {
+        Store store = new Store();
+        List<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Orange");
+        store.setProductList(list);
+        return store;
     }
 
     @RequestMapping("/user")
-    public String user() {
-        return "Name is Sunny";
+    public Student user() {
+        Student student = new Student();
+        student.setName("Sunny");
+        return student;
     }
 }
